7ce5deba2b5926595d588448e88fb694f1f2a9d4
S-Lombardi <silo.lombardi@gmail.com>
2023-07-06 18:58:52 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <title>PHP ToDo List JSON</title>
+</head>
+<body>
+    
+</body>
+</html>
