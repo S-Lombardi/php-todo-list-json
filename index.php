b0b7bd4227475d6ac6f708ccd941a08b55e52e72
S-Lombardi <silo.lombardi@gmail.com>
2023-07-06 20:02:56 +0200

    Creato file index con cdn di Bootstrap, Vue e Axios. Aggiunto file js

--- index.php
+++ index.php
@@ -8,10 +8,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <!-- Vue -->
+    <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <!-- Axios -->
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js"></script>
     <title>PHP ToDo List JSON</title>
 </head>
 <body>
-    
+    <div id="app">
+        {{prova}}
+
+    </div>
+
+
+
+    <script type="text/javascript" src="./js/script.js"></script>
 </body>
 </html>
---
+++ js/script.js
@@ -0,0 +1,10 @@
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+            prova: 'prova'
+        }
+    }
+
+}).mount('#app');
